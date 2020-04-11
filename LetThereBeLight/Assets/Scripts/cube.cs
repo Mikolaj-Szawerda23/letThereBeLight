a849205a97b4d63ea89ae92757501b50f1ba0437
Mikolaj-Szawerda23 <mikolaj.szawerda@wp.pl>
2020-04-11 23:42:44 +0200

    gaszenie_zapalanie

---
+++ LetThereBeLight/Assets/Scripts/cube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    [SerializeField]
+    private Material black;
+    [SerializeField]
+    private Material light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameMan.changeMaterial += changeToLight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void changeToLight()
+    {
+        GetComponent<MeshRenderer>().material = black;
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+    private void OnDisable()
+    {
+        gameMan.changeMaterial -= changeToLight;
+    }
+
+}
---
+++ LetThereBeLight/Assets/Scripts/gameMan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class gameMan : MonoBehaviour
+{
+    public static Action changeMaterial;
+    [SerializeField]
+    private LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            Shoot(Input.GetTouch(0).position);
+        }
+    }
+    void Shoot(Vector3 touch)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(touch);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit,mask))
+        {
+            changeMaterial?.Invoke();
+        }
+    }
+}
